2c95555af0f282e42358b8df9544bc2607aebda1
NARESH SINGH BISHT <nareshsinghankit1@gmail.com>
2024-08-12 22:34:01 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,10 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -26,10 +27,10 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
 });
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,6 +1,11 @@
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
 const app = express();
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:3000"],
